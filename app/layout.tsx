a2758d6b0aba097a967f32d858614ebfb7444d9d
Farhan Ahmed <145674138+FarhanRj389@users.noreply.github.com>
2025-11-13 21:42:09 +0500

    updates logo updates and other updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,24 +20,28 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <nav className="border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-          <div className="container flex h-14 items-center">
-            <Link href="/" className="mr-6 flex items-center space-x-2">
-            <Image src="/logo.png" alt="Groq PDF Assistant" width={32} height={32} />
-              <span className="font-bold text-xl">FASTIAN'S PDF Utility</span>
+        <nav className="bg-cyan-800 py-8 ">
+          <div className="container flex h-14 items-center justify-center">
+            <Link href="/" className="flex flex-col items-center space-x-2 ">
+              <Image src="/logo.png" alt="FASTIAN'S PDF Utility" width={100} height={100} />
+              <span className="font-bold text-xl text-white">FASTIAN'S PDF Utility</span>
             </Link>
-            <div className="flex flex-1 items-center justify-end space-x-4">
-              <Link href="/query">
-                <Button variant="ghost">Query</Button>
-              </Link>
-              <Link href="/summarize">
-                <Button variant="ghost">Summarize</Button>
-              </Link>
-              <Link href="/about">
-                <Button variant="ghost">About</Button>
-              </Link>
-            </div>
           </div>
+          <div className="flex flex-1 items-center justify-center space-x-4 pt-14">
+            <Link href="/">
+              <Button variant="ghost" className="text-white font-bold text-lg">Home</Button>
+            </Link>
+            <Link href="/query">
+              <Button variant="ghost" className="text-white font-bold text-lg">Query</Button>
+            </Link>
+            <Link href="/summarize">
+              <Button variant="ghost" className="text-white font-bold text-lg">Summarize</Button>
+            </Link>
+            <Link href="/about">
+              <Button variant="ghost" className="text-white font-bold text-lg">About</Button>
+            </Link>
+          </div>
+
         </nav>
         <main className="min-h-[calc(100vh-3.5rem)]">{children}</main>
       </body>
